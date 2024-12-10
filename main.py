813b9e861e671a40bb4726e38e9f291094b7f954
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-12-10 18:05:53 +0530

    .

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 import streamlit as st
-import cv2
+from streamlit_webrtc import webrtc_streamer
 
-def main():
-    st.set_page_config(page_title="Streamlit WebCam App")
-    st.title("Webcam Display Steamlit App")
-    st.caption("Powered by OpenCV, Streamlit")
-    cap = cv2.VideoCapture(0)
-    frame_placeholder = st.empty()
-    stop_button_pressed = st.button("Stop")
-    while cap.isOpened() and not stop_button_pressed:
-        ret, frame = cap.read()
-        if not ret:
-            st.write("Video Capture Ended")
-            break
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(frame,channels="RGB")
-        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+st.title("OpenCV Filters on Video Stream")
 
-if __name__ == "__main__":
-    main()
+webrtc_streamer(key="streamer", sendback_audio=False)
